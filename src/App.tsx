7bc89f3c420eef549add1aa75a6e59a5f0e91cd0
Amanda Kendal-Brown <amanda.kendal-brown@automox.com>
2024-11-07 14:05:57 -0700

    [WIP]

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.scss'
-
 import React from 'react'
 import styled from 'styled-components'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <p>test</p>
+    </div>
+  )
 }
 
 export default App
